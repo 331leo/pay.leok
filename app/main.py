c6bb4e83df357aed0b67cb8856131245667a006b
331leo <331leo@outlook.com>
2021-04-25 02:02:15 +0900

    feat: connect Tosspayments -> /pay

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+import os
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse, JSONResponse
 from fastapi.staticfiles import StaticFiles
@@ -16,5 +17,11 @@ templates = Jinja2Templates(directory="templates")
 async def route_root(request: Request):
     return {"ok": "OK"}
 
+@app.get("/pay", response_class=HTMLResponse)
+async def route_pay(request: Request):
+    return templates.TemplateResponse("pay.html.j2", {"request": request, "toss_ck": os.environ.get("PSTP-TOSS-CLIENT")})
 
+@app.get("/pay/{ordernum}", response_class=HTMLResponse)
+async def route_pay_with_ordernum(request: Request,ordernum):
+    return templates.TemplateResponse("pay.html.j2", {"request":request, "toss_ck": os.environ.get("PSTP-TOSS-CLIENT"), "ordernum": ordernum})
 app.include_router(router, prefix="/test")
